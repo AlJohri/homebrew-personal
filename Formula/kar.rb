b620841113f0ff0d89f1b8c3b00c5f54f262b4df
Al Johri <al.johri@gmail.com>
2020-01-12 11:06:57 -0500

    add coreutils dep and fix help.sh installation

--- Formula/kar.rb
+++ Formula/kar.rb
@@ -2,10 +2,11 @@ class Kar < Formula
   desc "Kar"
   url "https://github.com/AlJohri/kar.git", :using => GitDownloadStrategy
   version "0.0.2"
-  depends_on "bash"
+  depends_on "bash" # for bash associative arrays
+  depends_on "coreutils" # for realpath
 
   def install
-    libexec.install "kar"
+    libexec.install "kar", "help.sh"
     bin.install_symlink "#{libexec}/kar"
   end
 end
