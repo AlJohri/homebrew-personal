31e68e759db2f58c74870ae76146e4230dc46e0f
Al Johri <al.johri@gmail.com>
2020-01-12 08:56:49 -0500

    update kar formula

--- Formula/kar.rb
+++ Formula/kar.rb
@@ -1,7 +1,8 @@
 class Kar < Formula
   desc "Kar"
   url "https://github.com/AlJohri/kar.git", :using => GitDownloadStrategy
-  version "0.0.1"
+  version "0.0.2"
+  depends_on "bash"
 
   def install
     libexec.install "kar"
