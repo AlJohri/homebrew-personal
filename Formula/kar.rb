4c24989ebdcf6679a8c252a8a09a0a2eab063b1f
Al Johri <al.johri@gmail.com>
2020-01-14 19:25:16 -0500

    add new version of kar with python support

--- Formula/kar.rb
+++ Formula/kar.rb
@@ -1,12 +1,12 @@
 class Kar < Formula
   desc "Kar"
   url "https://github.com/AlJohri/kar.git", :using => GitDownloadStrategy
-  version "0.0.2"
+  version "0.0.3"
   depends_on "bash" # for bash associative arrays
   depends_on "coreutils" # for realpath
 
   def install
-    libexec.install "kar", "help.sh"
+    libexec.install "kar", "kar.sh", "kar.py", "help.sh"
     bin.install_symlink "#{libexec}/kar"
   end
 end
